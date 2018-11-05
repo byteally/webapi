5385369291784a2032c7814970fa79b4997d70c9
Sreenidhi <nair.sreenidhi@gmail.com>
2018-11-05 19:01:10 +0530

    depend on minimum version for CPP

--- webapi-docs/src/WebApi/Docs.hs
+++ webapi-docs/src/WebApi/Docs.hs
@@ -206,10 +206,10 @@ quoteFieldExp fld = do
 
 #if __GLASGOW_HASKELL__ >= 800
 instance fn ~ fn' => IsLabel (fn :: Symbol) (FName fn') where
-#if __GLASGOW_HASKELL__ >= 800
-  fromLabel = FN
-#else
-  fromLabel _ = FN
-#endif
   {-# INLINE fromLabel #-}
+#endif
+#if MIN_VERSION_base(4,10,1)
+  fromLabel = FN 
+#elif MIN_VERSION_base(4,9,1)          
+  fromLabel _ = FN                
 #endif
