a3d87e811cef87157af2a0d93f0c743285f16da4
Sreenidhi <nair.sreenidhi@gmail.com>
2019-02-06 21:47:43 +0530

    app for generating hask project

--- webapi-swagger/app/Main.hs
+++ webapi-swagger/app/Main.hs
@@ -1,6 +1,11 @@
 module Main where
 
--- import Lib
+import System.Environment
+import ContractGen
 
 main :: IO ()
-main = undefined
+main = do
+  args <- getArgs
+  case args of
+    [jsonp, outp] -> runCodeGen jsonp outp
+    _             -> putStrLn "Invalid usage. init-swagger-project <json-path> <output-path>"
