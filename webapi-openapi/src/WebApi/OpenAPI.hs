7dbdb5a5016db955ee78e1712541783a32ee52a1
pankajsijwali1 <pankajsijwali1@gmail.com>
2021-06-18 15:24:50 +0530

    handled some model-gen cases as needed for netsuite

--- webapi-openapi/src/WebApi/OpenAPI.hs
+++ webapi-openapi/src/WebApi/OpenAPI.hs
@@ -18,7 +18,7 @@ import Data.OpenApi
       Reference(Reference),
       Referenced(..),
       Schema(_schemaType, _schemaFormat, _schemaItems, _schemaRequired,
-             _schemaProperties),
+             _schemaProperties, _schemaOneOf),
       Definitions,
       Param(_paramName, _paramSchema),
       Operation(_operationParameters))
@@ -51,7 +51,7 @@ import GHC.SourceGen
 import GHC.Paths (libdir)
 import GHC ( runGhc )
 import Data.HashMap.Strict.InsOrd as HMO (toList,lookup)
-import Data.Text as T ( unpack, Text, append, splitAt, toUpper, take, pack, dropEnd, concat)
+import Data.Text as T ( unpack, Text, append, splitAt, toUpper, take, pack, dropEnd, concat, split, toLower)
 import Data.Text.IO as T (writeFile)
 import GhcPlugins(getDynFlags,mkVarOcc)
 import Control.Monad.State.Class ( MonadState(get), modify )
@@ -67,17 +67,18 @@ import System.FilePath.Posix
     ( (<.>), (</>), dropExtension, takeFileName, splitDirectories )
 import System.Directory ( createDirectoryIfMissing )
 import Data.List as L (delete)
-import Data.HashMap.Internal as HM (HashMap, singleton, unions, lookup)
+import Data.HashMap.Internal as HM (HashMap, singleton, unions, lookup, empty, insert)
 import Language.Haskell.TH (Extension(..))
-
+import Test.FitSpec.Utils(contained)
 
 data ModelGenState =
     ModelGenState { seenVars :: Set Text
                   , imports :: Set Text
                   , keywordsToAvoid :: Set Text
+                  , createdSums :: HashMap Text [Referenced Schema]
                   }
 
-data PkgConfig = 
+data PkgConfig =
     PkgConfig { authorName :: Text
               , email :: Text
               }
@@ -91,16 +92,16 @@ generateModels fp destFp = do
     oApi <- readOpenAPI fp
     let compSchemas =  _componentsSchemas . _openApiComponents $ oApi
         compParams = _componentsParameters . _openApiComponents $ oApi
-        oApiName =  "PetStore" --_infoTitle . _openApiInfo $ oApi
+        oApiName =  "NetSuites" --_infoTitle . _openApiInfo $ oApi
         modelList = evalState
                         (mapM createModelData (HMO.toList compSchemas))
-                        (ModelGenState S.empty S.empty (fromList keywords))
-        hsModuleModel = module' (Just modName) Nothing impsModel (namedTy:modelList)
+                        (ModelGenState S.empty S.empty (fromList keywords) HM.empty)
+        hsModuleModel = module' (Just modName) Nothing impsModel (namedTy: Prelude.concat modelList)
         (routeInfo,typeSynList) =  unzip . Prelude.concat $
                                       evalState
                                         (mapM (createTypeSynData compParams) (HMO.toList . _openApiPaths $ oApi))
-                                        (ModelGenState S.empty S.empty S.empty)
-        hsModuleTypeSyn = module' (Just typeSynName) Nothing impsTypeSyn (typeSynList ++ webApiInstance oApiName routeInfo)
+                                        (ModelGenState S.empty S.empty S.empty HM.empty)
+        hsModuleTypeSyn = module' (Just typeSynName) Nothing impsTypeSyn (Prelude.concat typeSynList ++ webApiInstance oApiName routeInfo)
     writeModule (pkgHome </> "src") (modName <.> "hs") es hsModuleModel
     writeModule (pkgHome </> "src") (typeSynName <.> "hs") es2 hsModuleTypeSyn
     writeCabal pkgName mkPkgConfig modName [typeSynName] pkgHome
@@ -119,12 +120,12 @@ generateModels fp destFp = do
                         (prefixCon "Field" [field $ var "a"]) []
           es = [TypeOperators,KindSignatures,DataKinds,DuplicateRecordFields]
           es2 = []
-          keywords = ["type"]
+          keywords = ["type","class"]
           pkgName = T.unpack . flip T.append "-models" . T.pack . dropExtension . takeFileName $ fp
           pkgHome = destFp </> pkgName
           modName = "OpenApiModels"
           typeSynName = "WebApiInstances"
-          dataTypeForOApi a = data' (textToOccNameStr a) [] [] []
+          dataTypeForOApi a = data' (textToOccNameStr . upperFirstChar $ a) [] [] []
           webApiInstance a b = [dataTypeForOApi a,
                                     instance' (var "WebApi" @@ var (textToRdrNameStr a))
                                               [tyFamInst "Apis" [var $ textToRdrNameStr a] (listPromotedTy (oneRoute <$> b))]]
@@ -132,7 +133,7 @@ generateModels fp destFp = do
 
 createTypeSynData ::
     (MonadState ModelGenState m) =>
-    Definitions Param -> (FilePath,PathItem) -> m [((Text,[Text]),HsDecl')]
+    Definitions Param -> (FilePath,PathItem) -> m [((Text,[Text]),[HsDecl'])]
 createTypeSynData compsParam  (fp,PathItem _ _ piGet piPut piPost piDelete piOptions piHead piPatch piTrace _ piParams) = do
     let paramsMap = refParamsToParams compsParam piParams
         commonParams =
@@ -165,26 +166,26 @@ createTypeSynData compsParam  (fp,PathItem _ _ piGet piPut piPost piDelete piOpt
 createTypSynonym ::
     (MonadState ModelGenState m) =>
     (Text ,[Either Text (Text, Maybe (Bool, Param))]) ->
-    m ((Text,[Text]),HsDecl')
+    m ((Text,[Text]),[HsDecl'])
 createTypSynonym (oName,params) = do
     varName <- mkUnseenVar (mkTypeSynName params (T.append oName "R"))
-    let typInfo = parseTypeSynInfo params
+    (typInfo,childTypes) <-unzip <$> mapM parseTypeSynInfo params
     return ((varName, [oName]), type' (textToOccNameStr varName)
-                                    []
-                                    (foldr1 (`op` ":/") typInfo))
+                                       []
+                                       (foldr1 (`op` ":/") typInfo): Prelude.concat childTypes)
 
 parseTypeSynInfo ::
-    [Either Text (Text, Maybe (Bool, Param))] ->
-    [HsType']
-parseTypeSynInfo = fmap
-                    (\case
-                      Left x -> stringTy $ T.unpack x
-                      Right (_, Nothing) -> error "Type Not Found"
-                      Right( _, Just (_,y)) ->
-                          case _paramSchema y of
-                             Nothing -> error "No Parameter Schema"
-                             Just s  ->  snd $ parseRecordFields ("",s) True
-                    )
+    (MonadState ModelGenState m) =>
+    Either Text (Text, Maybe (Bool, Param)) ->
+    m (HsType',[HsDecl'])
+parseTypeSynInfo (Left x) = return (stringTy $ T.unpack x,[])
+parseTypeSynInfo (Right (_,Nothing)) = error "Type Not Found"
+parseTypeSynInfo (Right (x,Just (_,y))) =
+    case _paramSchema y of
+        Nothing -> error "No Parameter Schema"
+        Just s  -> do
+            ((_,typ),childTypes) <- parseRecordFields (x,s) True
+            return (typ,childTypes)
 
 
 mkTypeSynName :: [Either Text (Text, Maybe (Bool, Param))] -> Text -> Text
@@ -201,7 +202,7 @@ mkTypeSynName a = T.append
 mkUnseenVar :: (Monad m, MonadState ModelGenState m) => Text -> m Text
 mkUnseenVar t = do
   ModelGenState { seenVars } <- get
-  let newV = go (0 :: Integer) seenVars t
+  let newV = go (0 :: Integer) seenVars (removeUnsupportedSymbols t)
   modify (updateSeenVars newV)
   pure newV
   where go x varSet el =
@@ -211,10 +212,12 @@ mkUnseenVar t = do
                                           (T.pack (T.unpack (T.dropEnd (length . show $ x-1) el) ++ show x))) else el
 
 updateSeenVars :: Text -> ModelGenState -> ModelGenState
-updateSeenVars v (ModelGenState x y z) =
-  ModelGenState (insert v x) y z
+updateSeenVars v (ModelGenState x y z w) =
+  ModelGenState ( S.insert v x) y z w
 
-
+updateSumTypes :: (Text,[Referenced Schema]) -> ModelGenState -> ModelGenState
+updateSumTypes (a,b) (ModelGenState w x y z) =
+    ModelGenState w x y (HM.insert a b z)
 
 handleOverridenParams ::
     Definitions Param ->
@@ -272,10 +275,10 @@ writeModule destFp fName es hsModule = do
     let fileContent = concatMap ppExtension es <> showSDoc dynFlags (ppr hsModule)
     txt <- ormolu defaultConfig { cfgCheckIdempotence = True } "" fileContent
     createDirectoryIfMissing True destFp
-    T.writeFile (destFp </> fName) txt
+    T.writeFile (destFp </> fName) txt --(T.pack fileContent)
 
 writeCabal :: String -> PkgConfig -> String -> [String] -> FilePath ->IO ()
-writeCabal pkgName (PkgConfig aName aEmail) modName exposMods pkgHome = do
+writeCabal pkgName (PkgConfig aName aEmail) modName exposMods pkgHome =
     callCommand ("cd " ++ pkgHome ++ " ; " ++ str)
     where str = "cabal init --non-interactive --overwrite"
                  ++ " --package-name="
@@ -295,51 +298,92 @@ ppExtension e = "{-# LANGUAGE " <> show e <> " #-}\n"
 
 createModelData ::
     (MonadState ModelGenState m) =>
-    (Text,Schema) -> m HsDecl'
+    (Text,Schema) -> m [HsDecl']
 createModelData (dName,dSchema) = do
     let reqParams =  _schemaRequired dSchema
-        rFields = (\(x,y) -> parseRecordFields (x,y) (x `elem` reqParams)) <$> (HMO.toList . _schemaProperties $ dSchema)
+    unseenVar <- mkUnseenVar (upperFirstChar dName)
+    (rFields,childTypes) <- unzip <$> mapM (\(x,y) -> parseRecordFields (x,y) (x `elem` reqParams))  (HMO.toList . _schemaProperties $ dSchema)
     ModelGenState { keywordsToAvoid } <- get
-    let frFields = bimap textToOccNameStr field .
+    let frFields = bimap (textToOccNameStr . lowerFirstChar) field .
                         (\(x,y)-> (if member x keywordsToAvoid
                                    then T.append x "_"
                                    else x,y)) <$> rFields
-    return (data' (textToOccNameStr dName) [] [recordCon (textToOccNameStr dName) frFields] [])
+    return $ Prelude.concat childTypes ++ [data' (textToOccNameStr unseenVar) [] [recordCon (textToOccNameStr . upperFirstChar $ dName) frFields] []]
 
 parseRecordFields ::
-    (Text, Referenced Schema) -> Bool -> (Text,HsType')
+    (MonadState ModelGenState m) =>
+    (Text, Referenced Schema) -> Bool -> m ((Text,HsType'),[HsDecl'])
 parseRecordFields (dName,Ref (Reference x)) isReq =
-    (dName,if isReq
-                   then var $ textToRdrNameStr x
-                   else var "Maybe" @@ var ( textToRdrNameStr x))
+    return ((dName,if isReq
+                   then var $ textToRdrNameStr . upperFirstChar $ x
+                   else var "Maybe" @@ var ( textToRdrNameStr . upperFirstChar $ x)),[])
 parseRecordFields (dName,Inline dSchema) isReq = parseInlineFields (_schemaType dSchema) dName dSchema isReq
 
 parseInlineFields ::
-    Maybe OpenApiType -> Text -> Schema -> Bool -> (Text,HsType' )
+    (MonadState ModelGenState m) =>
+    Maybe OpenApiType -> Text -> Schema -> Bool -> m ((Text,HsType'),[HsDecl'])
 parseInlineFields (Just OpenApiString) dName _dSchema isReq =
-    (dName, if isReq then var "Text" else var "Maybe" @@ var "Text")
+    return ((dName, if isReq then var "Text" else var "Maybe" @@ var "Text"),[])
 parseInlineFields (Just OpenApiNumber ) dName _dSchema isReq =
-    (dName, if isReq then var "Double" else var "Maybe" @@ var "Double")
+    return ((dName, if isReq then var "Double" else var "Maybe" @@ var "Double"),[])
 parseInlineFields (Just OpenApiInteger) dName dSchema isReq =
-    (dName, if isReq then var $ textToRdrNameStr parsedInt else var "Maybe" @@ var (textToRdrNameStr parsedInt))
+    return ((dName, if isReq then var $ textToRdrNameStr parsedInt else var "Maybe" @@ var (textToRdrNameStr parsedInt)),[])
     where parsedInt = parseIntegerFld (_schemaFormat dSchema)
 parseInlineFields (Just OpenApiBoolean) dName _dSchema isReq =
-    (dName,if isReq then var "Bool" else var "Maybe" @@ var "Bool")
+    return ((dName,if isReq then var "Bool" else var "Maybe" @@ var "Bool"),[])
 parseInlineFields (Just OpenApiArray ) dName dSchema _isReq =
     case _schemaItems dSchema of
         Nothing -> error "No _schemaItems value for Array"
-        Just (OpenApiItemsObject sch) ->
-            let (dName2,dType) = parseRecordFields (dName,sch) True
-             in (dName2,listTy dType)
+        Just (OpenApiItemsObject sch) -> do
+            ((dName2,dType),childTypes) <- parseRecordFields (dName,sch) True
+            return ((dName2,listTy dType),childTypes)
         Just (OpenApiItemsArray _) -> error "OpenApiItemsArray Array type"
 parseInlineFields (Just OpenApiNull ) _dName _dSchema _isReq =
     error "Null OpenApi Type"
 parseInlineFields (Just OpenApiObject ) dName dSchema isReq = do
-    (dName,if isReq then typeTuple else var "Maybe" @@ typeTuple)
+    (childInlines,childTypes) <- unzip <$> mapM (\(x,y) -> parseRecordFields (x,y) (x `elem` reqParams)) (HMO.toList . _schemaProperties $ dSchema)
+    let typeTuple =  tuple $ (\(x,y)-> op (stringTy $ T.unpack x) ":::" y) <$> childInlines
+    return ((dName,if isReq then typeTuple else var "Maybe" @@ typeTuple),Prelude.concat childTypes)
     where reqParams =  _schemaRequired dSchema
-          childInlines = (\(x,y) -> parseRecordFields (x,y) (x `elem` reqParams)) <$> (HMO.toList . _schemaProperties $ dSchema)
-          typeTuple =  tuple $ (\(x,y)-> op (stringTy $ T.unpack x) ":::" y) <$> childInlines
-parseInlineFields Nothing _dName _dSchema _isReq = error "No Type Defined"
+
+parseInlineFields Nothing dName dSchema isReq =
+    case _schemaOneOf dSchema of
+        Nothing -> let props = HMO.toList . _schemaProperties $ dSchema
+                   in case props of
+                       [] -> error "Unexpected Schema type"
+                       _ -> parseInlineFields (Just OpenApiObject) dName dSchema isReq
+        Just x -> do
+            ModelGenState { createdSums } <- get
+            (isReg,vName) <- registerSumType (upperFirstChar dName) x createdSums
+            if isReg
+            then return ((dName,if isReq then var (textToRdrNameStr vName) else var "Maybe" @@ var (textToRdrNameStr vName)), [])
+            else do
+                let newVarList = mkNewVariables vName (length x)
+                unSeenVars <- mapM mkUnseenVar newVarList
+                (typList,childTypes) <- unzip <$> mapM (\(a,b) -> parseRecordFields (a,b) True)  (zip unSeenVars x)
+                let oneOfTyp = data' (textToOccNameStr vName) [] ((\(a,b) -> prefixCon (textToOccNameStr a) [field b]) <$> typList) []
+                return ((dName,if isReq then var (textToRdrNameStr vName) else var "Maybe" @@ var (textToRdrNameStr vName)), oneOfTyp : Prelude.concat childTypes)
+
+registerSumType :: 
+    MonadState ModelGenState m =>
+     Text -> [Referenced Schema] -> HashMap Text [Referenced Schema] -> m (Bool,Text)
+registerSumType tName schemaList hm =
+    case HM.lookup tName hm of
+        Nothing -> do
+            unseenVar <- mkUnseenVar tName
+            modify (updateSumTypes (unseenVar,schemaList))
+            return (False,unseenVar)
+        Just x -> do
+            if contained x schemaList && contained schemaList x
+             then return (True,tName)
+             else do
+                 unseenVar <- mkUnseenVar tName
+                 modify (updateSumTypes (unseenVar,schemaList))
+                 return (False,unseenVar)
+
+mkNewVariables :: Text -> Int -> [Text]
+mkNewVariables _dName 0 = []
+mkNewVariables dName x =  mkNewVariables dName (x-1)  ++ [T.concat [dName,"C", T.pack . show $ x]]
 
 parseIntegerFld :: Maybe Text -> Text
 parseIntegerFld (Just x) = let y = upperFirstChar x
@@ -348,15 +392,27 @@ parseIntegerFld (Just x) = let y = upperFirstChar x
                                else error "Invalid Integer Format"
 parseIntegerFld Nothing = "Int"
 
+removeUnsupportedSymbols :: Text -> Text
+removeUnsupportedSymbols = removeSymbol '-' . removeSymbol '!' . removeSymbol ':'
 
 textToOccNameStr :: Text -> OccNameStr
-textToOccNameStr = occNameToStr . mkVarOcc . unpack
+textToOccNameStr = occNameToStr . mkVarOcc . unpack . removeUnsupportedSymbols
 
 textToRdrNameStr :: Text -> RdrNameStr
-textToRdrNameStr = fromString . T.unpack
+textToRdrNameStr = fromString . T.unpack . removeUnsupportedSymbols
 
 upperFirstChar :: Text -> Text
 upperFirstChar x = let (fir,res) = T.splitAt 1 x in append (toUpper fir) res
+
+lowerFirstChar :: Text -> Text
+lowerFirstChar x = let (fir,res) = T.splitAt 1 x in append (toLower fir) res
+
+removeSymbol :: Char -> Text -> Text
+removeSymbol c x = T.concat $
+    let elems = T.split (== c) x
+        fir = head elems
+    in fir : (upperFirstChar <$> tail elems)
+
 
 retOApi :: Maybe p -> p
 retOApi (Just x) = x
