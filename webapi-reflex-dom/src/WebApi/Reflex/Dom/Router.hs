13f4b6bcb99be8782ed50c2cf9e6fc6db6268d87
Magesh <magesh85@gmail.com>
2022-04-20 01:49:44 +0530

    Added page400 support and fixed the failing test code

--- webapi-reflex-dom/src/WebApi/Reflex/Dom/Router.hs
+++ webapi-reflex-dom/src/WebApi/Reflex/Dom/Router.hs
@@ -20,7 +20,8 @@
 {-# LANGUAGE TypeApplications      #-}
 {-# LANGUAGE AllowAmbiguousTypes   #-}
 {-# LANGUAGE TypeFamilyDependencies #-}
-{-# LANGUAGE TupleSections          #-}
+{-# LANGUAGE DerivingStrategies       #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 
 module WebApi.Reflex.Dom.Router
   ( respond
@@ -69,6 +70,7 @@ import qualified Data.ByteString as BS
 import qualified Data.ByteString.Char8 as Char8
 import Data.Typeable
 import Data.Maybe
+import Control.Exception (SomeException (..), Exception(..))
 
 -- Compact Start
 import           GHC.Records
@@ -90,7 +92,7 @@ class UIHandler w (t :: *) s m r where
 
 newtype UIRequestRep =
   UIRequestRep { getUIRequestRep :: TypeRep
-               } deriving (Show, Eq)
+               } deriving newtype (Show, Eq)
 
 compactUIServer :: forall api m server t. server t (RouteT t m) -> CompactUIServer api (server t (RouteT t m))
 compactUIServer = CompactUIServer
@@ -111,56 +113,56 @@ data DomResponse =
 type ReflexDomApplication t m = Dynamic t DomRequest -> Dynamic t (RouteResult (UIRequestRep, m (Event t DomResponse)))
 
 class Monad w => Router (w :: * -> *) (t :: *) (server :: *) (r :: k) (pr :: (*, [*])) where
-  route :: Proxy '(r, t) -> server -> ParsedRoute pr -> ReflexDomApplication t w
+  route :: Proxy '(r, t) -> server -> ParsedRoute pr -> (Dynamic t [ParamErr] -> w ()) -> ReflexDomApplication t w
 
 instance ( SingMethod (m :: *)
          , Router w t s r '(Dom m, '[])
          , MonadWidget t w
          ) => Router w t s (W.Route '[Dom m] r) pr where
-  route _ _s parsedRoute reqDyn =
-    route (Proxy :: Proxy '(r, t)) _s (Nil (Proxy :: Proxy (Dom m))) reqDyn
+  route _ _s parsedRoute page400 reqDyn =
+    route (Proxy :: Proxy '(r, t)) _s (Nil (Proxy :: Proxy (Dom m))) page400 reqDyn
 
 instance ( MonadWidget t w ) => Router w t s (W.Route '[] r) pr where
-  route _ _s _ _ = pure NotMatched
+  route _ _s _ _ _ = pure NotMatched
 
 instance
   ( Router w t s route pr
   , Router w t s routes pr
   , Reflex t
   ) => Router w t s ((route :: *) ': routes) pr where
-  route _ _s parsedRoute request =
+  route _ _s parsedRoute page400 request =
     zipDynWith (<>)
-               (route (Proxy :: Proxy '(route, t)) _s parsedRoute request)
-               (route (Proxy :: Proxy '(routes, t)) _s parsedRoute request)
+               (route (Proxy :: Proxy '(route, t)) _s parsedRoute page400 request)
+               (route (Proxy :: Proxy '(routes, t)) _s parsedRoute page400 request)
 
 instance (Monad w, Reflex t) => Router w t s '[] pr where
-  route _ _s _ _ = pure NotMatched
+  route _ _s _ _ _ = pure NotMatched
 
 instance (Monad w, Router w t s rest '(m, pp :++ '[Namespace ns])) => Router w t s ((ns :: *) :// (rest :: *)) '(m, pp) where
-  route _ _s parsedRoute request =
-    route (Proxy :: Proxy '(rest, t)) _s (snocParsedRoute parsedRoute $ NSPiece (Proxy :: Proxy ns)) request
+  route _ _s parsedRoute page400 request =
+    route (Proxy :: Proxy '(rest, t)) _s (snocParsedRoute parsedRoute $ NSPiece (Proxy :: Proxy ns)) page400 request
 
 instance (Monad w, Router w t s (MarkDyn rest) '(m, (pp :++ '[DynamicPiece piece])), DecodeParam piece, Reflex t)
                       => Router w t s ((piece :: *) :/ (rest :: *)) '(m, pp) where
-  route _ _s parsedRoute reqDyn = do
+  route _ _s parsedRoute page400 reqDyn = do
     reqDyn >>= go
 
     where go req = do
             case pathInfo req of
               (lpth : rpths)  -> case (decodeParam (encodeUtf8 lpth) :: Maybe piece) of
-                Just dynPiece -> route (Proxy :: Proxy '((MarkDyn rest), t)) _s (snocParsedRoute parsedRoute $ DPiece dynPiece) (fmap (\r -> r {pathInfo = rpths}) reqDyn)
+                Just dynPiece -> route (Proxy :: Proxy '((MarkDyn rest), t)) _s (snocParsedRoute parsedRoute $ DPiece dynPiece) page400 (fmap (\r -> r {pathInfo = rpths}) reqDyn)
                 Nothing -> pure NotMatched
               _ -> pure NotMatched
 
 instance (Reflex t, Monad w, Router w t s (MarkDyn rest) '(m, (pp :++ '[StaticPiece piece])), KnownSymbol piece)
   => Router w t s ((piece :: Symbol) :/ (rest :: *)) '(m, pp) where
-  route _ _s parsedRoute reqDyn =
+  route _ _s parsedRoute page400 reqDyn =
     reqDyn >>= go
 
     where
       go req = do
         case pathInfo req of
-          (lpth : rpths) | lpieceTxt == lpth -> route (Proxy :: Proxy '((MarkDyn rest), t)) _s (snocParsedRoute parsedRoute $ SPiece (Proxy :: Proxy piece)) (fmap (\r -> r {pathInfo = rpths}) reqDyn)
+          (lpth : rpths) | lpieceTxt == lpth -> route (Proxy :: Proxy '((MarkDyn rest), t)) _s (snocParsedRoute parsedRoute $ SPiece (Proxy :: Proxy piece)) page400 (fmap (\r -> r {pathInfo = rpths}) reqDyn)
           _ -> pure NotMatched
 
         where lpieceTxt = symTxt (Proxy :: Proxy piece)
@@ -193,7 +195,7 @@ instance ( KnownSymbol lpiece
          , HeaderIn m route ~ ()
          , RequestBody m route ~ '[]
          ) => Router w t s ((lpiece :: Symbol) :/ (rpiece :: Symbol)) '(m, pp) where
-  route _ serv parsedRoute reqDyn = fmap go reqDyn
+  route _ serv parsedRoute page400 reqDyn = fmap go reqDyn
 
       where go req = do
               case pathInfo req of
@@ -210,7 +212,7 @@ instance ( KnownSymbol lpiece
                     pRoute = snocParsedRoute (snocParsedRoute parsedRoute $ SPiece (Proxy :: Proxy lpiece)) $ SPiece (Proxy :: Proxy rpiece)
                     pathPar = fromParsedRoute pRoute
                     getResponse =
-                      toDomResponse reqDyn (fromDomRequest @m @route reqDyn (fromParsedRoute pRoute) (apiHandler serv))
+                      toDomResponse reqDyn (fromDomRequest @m @route reqDyn (fromParsedRoute pRoute) (apiHandler serv) page400)
 
 instance ( KnownSymbol rpiece
          , paths ~ (pp :++ '[DynamicPiece lpiece, StaticPiece rpiece])
@@ -238,7 +240,7 @@ instance ( KnownSymbol rpiece
          , HeaderIn m route ~ ()
          , RequestBody m route ~ '[]
          ) => Router w t s ((lpiece :: *) :/ (rpiece :: Symbol)) '(m, pp) where
-  route _ serv parsedRoute reqDyn = fmap go reqDyn
+  route _ serv parsedRoute page400 reqDyn = fmap go reqDyn
 
     where go req = do
             case pathInfo req of
@@ -251,7 +253,7 @@ instance ( KnownSymbol rpiece
                 getResponse dynVal =
                   let pRoute :: ParsedRoute '(m, paths)
                       pRoute = snocParsedRoute (snocParsedRoute parsedRoute $ DPiece dynVal) $ SPiece (Proxy :: Proxy rpiece)
-                  in toDomResponse reqDyn $ fromDomRequest @m @route reqDyn (fromParsedRoute pRoute) (apiHandler serv)
+                  in toDomResponse reqDyn $ fromDomRequest @m @route reqDyn (fromParsedRoute pRoute) (apiHandler serv) page400
 
 instance ( route ~ (FromPieces (pp :++ '[DynamicPiece t0]))
          , UIHandler w t s m route
@@ -276,7 +278,7 @@ instance ( route ~ (FromPieces (pp :++ '[DynamicPiece t0]))
          , HeaderIn m route ~ ()
          , RequestBody m route ~ '[]
          ) => Router w t s (DynamicPiece t0) '(m, pp) where
-  route _ serv parsedRoute reqDyn = fmap go reqDyn
+  route _ serv parsedRoute page400 reqDyn = fmap go reqDyn
 
     where go req = do
             case pathInfo req of
@@ -290,7 +292,7 @@ instance ( route ~ (FromPieces (pp :++ '[DynamicPiece t0]))
                     pRoute :: ParsedRoute '(m, (pp :++ '[DynamicPiece t0]))
                     pRoute = snocParsedRoute parsedRoute $ DPiece dynVal
                   in toDomResponse reqDyn
-                     $ fromDomRequest @m @route reqDyn (fromParsedRoute pRoute) (apiHandler serv)
+                     $ fromDomRequest @m @route reqDyn (fromParsedRoute pRoute) (apiHandler serv) page400
 
 instance ( PathParam m (ns :// piece) ~ ()
          , ParamErrToApiErr (ApiErr m (ns :// piece))
@@ -317,7 +319,7 @@ instance ( PathParam m (ns :// piece) ~ ()
          , HeaderIn m route ~ ()
          , RequestBody m route ~ '[]
          ) => Router w t s ((ns :: *) :// (piece :: Symbol)) '(m, pp) where
-  route _ serv _ reqDyn = fmap go reqDyn
+  route _ serv _ page400 reqDyn = fmap go reqDyn
 
     where go req = do
             case pathInfo req of
@@ -327,14 +329,14 @@ instance ( PathParam m (ns :// piece) ~ ()
 
               where
                 getResponse = toDomResponse reqDyn
-                  $ fromDomRequest @m @route reqDyn () (apiHandler serv)
+                  $ fromDomRequest @m @route reqDyn () (apiHandler serv) page400
 
 router ::
   forall apis server m t.
   ( Router m t server apis '(CUSTOM "", '[])
   , MonadWidget t m
-  ) => Proxy apis -> server -> ReflexDomApplication t m
-router _ s = route (Proxy :: Proxy '(apis, t)) s emptyParsedRoutes
+  ) => Proxy apis -> server -> (Dynamic t [ParamErr] -> m ()) -> ReflexDomApplication t m
+router _ s page400 = route (Proxy :: Proxy '(apis, t)) s emptyParsedRoutes page400
 
 fromDomRequest :: forall m r w t.
   ( Functor w
@@ -350,16 +352,17 @@ fromDomRequest :: forall m r w t.
   Dynamic t DomRequest ->
   PathParam m r ->
   (Dynamic t (Request m r) -> w (Response m r)) ->
+  (Dynamic t [ParamErr] ->  w ()) ->
   w (Event t (Response m r))
-fromDomRequest domReqDyn pp handlerFn = do
+fromDomRequest domReqDyn pp handlerFn page400 = do
   let
     reqValDyn = fmap toWebApiReqVal domReqDyn
   factoredErrSuccDyn <- eitherDyn reqValDyn
   -- Handles toggling of params decoding status between success & failure within same route.
   networkView $ ffor factoredErrSuccDyn $ \case
     Left errs -> do
-      display errs 
-      pure (undefined :: Response m r)
+      page400 errs
+      pure (Failure $ Right $ OtherError $ toException $ BadRequestError)
     Right waReqDyn -> handlerFn waReqDyn
   where
     toWebApiReqVal :: DomRequest -> Either [ParamErr] (Request m r)
@@ -395,12 +398,19 @@ toDomResponse _domReq res =
   where
     go (Success _ ao _ _) = DomSuccess (mkUIRequestRep (undefined :: UIRequest m r))
     go (Failure (Left (ApiError { err }))) = DomFailure (Right "")
-    go (Failure _) = DomFailure (Right "")
+    go (Failure (Right (OtherError se))) = case fromException se of
+      Just BadRequestError -> DomFailure (Right "Bad Request")
+      _ -> DomFailure (Right "")
 
 apiHandler ::
   ( UIHandler w t s m r
   ) => s -> Dynamic t (Request m r) -> w (Response m r)
 apiHandler = handler
+
+data BadRequestError = BadRequestError
+  deriving (Show)
+
+instance Exception BadRequestError
 
 toUIApplication ::
   forall t app m r meth ac mp.
@@ -452,7 +462,6 @@ toUIApplication r@UIRequest { uiPathParam, uiQueryParam } page404 app = withPath
     go0 =
         map (\(k, v) -> (T.encodeUtf8 k, pure $ T.encodeUtf8 v))
 
-
 uiApp :: forall (t :: *) server m app r meth ac mp.
   ( MonadWidget t m
   , MkPathFormatString (app :// r)
@@ -462,15 +471,16 @@ uiApp :: forall (t :: *) server m app r meth ac mp.
   , app ~ (UIInterface server)
   , ('ApiMount ac mp) ~ MountPoint app
   , KnownSymbol mp
-  ) => UIRequest meth (app :// r)
-    -> Maybe (Event t (AppLink (UIInterface server)))
-    -> RouteT t m ()
+  ) => UIRequest meth (app :// r) -- ^ Default route
+    -> Maybe (Event t (AppLink (UIInterface server))) -- ^ Global Nav
+    -> RouteT t m () -- ^ Page 404
+    -> (Dynamic t [ParamErr] -> RouteT t m ()) -- ^ Page 400
     -> server
     -> m (Event t DomResponse)
-uiApp def globalNavMay page404 server =
+uiApp def globalNavMay page404 page400 server =
   routeApp "" $ do
   maybe (pure ()) navigateToAppLink globalNavMay
-  toUIApplication def page404 $ router (Proxy :: Proxy (Apis (UIInterface server))) server
+  toUIApplication def page404 $ router (Proxy :: Proxy (Apis (UIInterface server))) server page400
 
 
 -- Shared: Server Router
