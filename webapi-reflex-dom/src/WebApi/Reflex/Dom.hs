4ab35098ba4882295b2a476d5a673a7082ba83e7
Magesh <magesh85@gmail.com>
2022-05-19 13:54:37 +0530

    Add networkViewByParam

--- webapi-reflex-dom/src/WebApi/Reflex/Dom.hs
+++ webapi-reflex-dom/src/WebApi/Reflex/Dom.hs
@@ -12,3 +12,20 @@ import WebApi.Reflex.Dom.Router
 import WebApi.Param hiding ( link )
 import Reflex.Dom.Contrib.MonadRouted
 import Reflex.Dom.Contrib.Router
+import Reflex hiding (Request, Response)
+import Reflex.Network
+import Control.Monad.Fix
+
+-- ^ `networkViewByParam` hold the view till param values selected from Request remains same
+networkViewByParam ::
+  ( Eq par
+  , NotReady t m
+  , Adjustable t m
+  , PostBuild t m
+  , MonadHold t m
+  , MonadFix m
+  ) => (Request meth r -> par) -- ^ Param selector
+  -> Dynamic t (Request meth r) -- ^ Request Dyn of the handler
+  -> (par -> m a) -- ^ view to be rendered until param values changes
+  -> m (Event t a)
+networkViewByParam sel reqDyn hand = networkView . (fmap hand) =<< (holdUniqDyn $ fmap sel reqDyn)
